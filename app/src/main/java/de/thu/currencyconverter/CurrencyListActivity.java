0812dc3746f281064cea0dccae34509ec4062e5f
bircni <nicolas@bircks.eu>
2022-11-03 23:21:27 +0100

    added toolbar and backbutton to maps

--- app/src/main/java/de/thu/currencyconverter/CurrencyListActivity.java
+++ app/src/main/java/de/thu/currencyconverter/CurrencyListActivity.java
@@ -3,9 +3,12 @@ package de.thu.currencyconverter;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.Arrays;
 
@@ -26,5 +29,19 @@ public class CurrencyListActivity extends AppCompatActivity {
             startActivity(i);
 
         });
+        Toolbar toolbar_list = (Toolbar)findViewById(R.id.toolbar_list);
+        setSupportActionBar(toolbar_list);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
